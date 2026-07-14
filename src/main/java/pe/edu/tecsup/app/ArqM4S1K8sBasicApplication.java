cbcaa05918e9c35baee71a07443e1743a3075070
jgomezz <jgomezz@gmail.com>
2026-07-13 20:43:40 -0500

    update change

---
+++ src/main/java/pe/edu/tecsup/app/ArqM4S1K8sBasicApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArqM4S1K8sBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArqM4S1K8sBasicApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/tecsup/app/ArqM4S1K8sBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.tecsup.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArqM4S1K8sBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
